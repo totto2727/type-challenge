3a792922c47a6ee2a33a6dad5229e96e12e1aeaa
totto2727 <kaihatu.totto2727@gmail.com>
2022-09-20 13:14:13 +0900

    00898よくわからない…

---
+++ 00533-easy-concat.ts
@@ -0,0 +1,18 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils.ts";
+
+type cases = [
+  Expect<Equal<Concat<[], []>, []>>,
+  Expect<Equal<Concat<[], [1]>, [1]>>,
+  Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
+  Expect<
+    Equal<
+      Concat<["1", 2, "3"], [false, boolean, "4"]>,
+      ["1", 2, "3", false, boolean, "4"]
+    >
+  >,
+];
+
+// ============= Your Code Here =============
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U];
+type A = Concat<[1, 2, 3], [4, 5, 6]>;
